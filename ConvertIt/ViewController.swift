a67d0829a42140f84d14a842d779a3f5a428bb68
Kim-An Quinn <kiman.quinn@bc.edu>
2019-09-27 12:04:25 -0400

    6.3 completed: added components method to strip string

--- ConvertIt/ViewController.swift
+++ ConvertIt/ViewController.swift
@@ -21,6 +21,9 @@ class ViewController: UIViewController {
                         "yards to meters",
                         "meters to feet",
                         "meters to yards"]
+    
+    var fromUnits = ""
+    var toUnits = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,7 +49,11 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource{
         return formulaArray[row]
     }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        fromUnitsLabel.text = formulaArray[row]
+        let unitsArray = formulaArray[row].components(separatedBy: " to ")
+        fromUnits = unitsArray[0]
+        toUnits = unitsArray[1]
+        fromUnitsLabel.text = fromUnits
+        resultsLabel.text = toUnits
     }
     
 }
